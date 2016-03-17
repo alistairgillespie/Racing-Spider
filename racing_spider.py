8a8d6c5b006ea4b245e1b22ca23190e30842b4b0
WilsonAus <gillaw06@gmail.com>
2016-03-17 21:33:33 +1100

    Fixed clash of commits

--- racing_spider.py
+++ racing_spider.py
@@ -11,17 +11,6 @@ class RacingSpider(scrapy.Spider):
     allowed_domains = ['racenet.com.au']
     start_urls = ['https://www.racenet.com.au/horse-racing-results/Albury/2016-03-17']
 
-<<<<<<< HEAD
-def parse(self, response):
-	for table in response.xpath('//table[@class="tblLatestHorseResults"]'):
-	    rows = response.xpath('.//tr[@class="tr_full_res_runner"]')
-	    for row in rows:
-	        item = PopulationItem()
-		item['fin'] = row.xpath('.//td[@class="first"]').extract()
-		item['runner'] = row.xpath('td[3]/text()').extract()
-		yield item
-        
-=======
     def parse(self, response):
     	for table in response.xpath('//table[@class="tblLatestHorseResults"]'):
 		    rows = response.xpath('.//tr[@class="tr_full_res_runner"]')
@@ -32,7 +21,6 @@ def parse(self, response):
 			yield item
 	        
 
->>>>>>> FETCH_HEAD
         
 
         
