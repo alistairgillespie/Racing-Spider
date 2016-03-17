89bd04281704a02e1c8e8b70d1dbed69e76d7cc4
WilsonAus <gillaw06@gmail.com>
2016-03-17 21:26:53 +1100

    Revert "Indentation"
    This reverts commit 7b88981190a3b18d76076ec80fa8de6964bcc592.

--- racing_spider.py
+++ racing_spider.py
@@ -1,24 +1,24 @@
 import scrapy
 
-
 class RacingItem(scrapy.Item):
     fin = scrapy.Field()
     runner = scrapy.Field()
 
 
 class RacingSpider(scrapy.Spider):
+    name = 'racing'
     name = 'racenet.com.au'
     allowed_domains = ['racenet.com.au']
     start_urls = ['https://www.racenet.com.au/horse-racing-results/Albury/2016-03-17']
 
-	def parse(self, response):
-		for table in response.xpath('//table[@class="tblLatestHorseResults"]'):
-		    rows = response.xpath('.//tr[@class="tr_full_res_runner"]')
-		    for row in rows:
-		        item = PopulationItem()
-			item['fin'] = row.xpath('.//td[@class="first"]').extract()
-			item['runner'] = row.xpath('td[3]/text()').extract()
-			yield item
+def parse(self, response):
+	for table in response.xpath('//table[@class="tblLatestHorseResults"]'):
+	    rows = response.xpath('.//tr[@class="tr_full_res_runner"]')
+	    for row in rows:
+	        item = PopulationItem()
+		item['fin'] = row.xpath('.//td[@class="first"]').extract()
+		item['runner'] = row.xpath('td[3]/text()').extract()
+		yield item
         
         
 
