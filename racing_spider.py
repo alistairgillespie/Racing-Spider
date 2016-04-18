295686d4ebbc5fb776e98a06e89ab24df6dd556a
Alistair <alistair@howlingweb.com.au>
2016-04-19 07:44:35 +1000

    Fixed duplicate return and tidied up formatting

--- racing_spider.py
+++ racing_spider.py
@@ -3,26 +3,20 @@ import scrapy
 class RacingItem(scrapy.Item):
     fin = scrapy.Field()
     runner = scrapy.Field()
-    number = scrapy.Field()
+    odds = scrapy.Field()
 
 
 class RacingSpider(scrapy.Spider):
     name = 'racenet.com.au'
     allowed_domains = ['racenet.com.au']
-    start_urls = ['https://www.racenet.com.au/horse-racing-results/Albury/2016-03-17']
+    start_urls = ['https://www.racenet.com.au/horse-racing-results/']
     
     def parse(self, response):
-    	for href in response.css('.res_meeting_link a::attr(href)'):
-        	full_url = response.urljoin(href.extract())
-		yield scrapy.Request(full_url, callback=self.parse_results)
-    
-
-    def parse_results(self, response):
-    	for table in response.xpath('//table[@class="tblLatestHorseResults"]'):
-		    rows = response.xpath('.//tr[@class="tr_full_res_runner"]')
+    	for table in response.xpath('.//table[@class="tblLatestHorseResults"]'):
+		    rows = table.xpath('.//tr[@class="tr_res_runner"]')
 		    for row in rows:
 		        item = RacingItem()
-			item['fin'] = row.xpath('.//td[@class="first"]/text()').extract()
-			item['runner'] = row.xpath('.//td[3]/a/text()').extract()
-			item['number'] = row.xpath('.//td[3]/text()').extract()
-			yield item
+		        item['fin'] = row.xpath('.//td[@class="first"]/text()').extract()
+		        item['runner'] = row.xpath('.//td[2]/a[@class="link_red bold"]/text()').extract()
+		        item['odds'] = row.xpath('.//td[@class="res_odds sb res_td_light last"]/span/text()').extract()
+		        yield item
